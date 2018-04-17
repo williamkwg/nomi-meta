7b013c5c8d901960764bc214aad6f42d4f9c9be1
liuxiaoli.liu <liuxiaoli.liu>
2018-04-17 16:54:34 +0800

    测试

--- index.js
+++ index.js
@@ -2,36 +2,26 @@
 * meta middleware, should be the first middleware
 */
 'use strict';
-
-const defaultOption = {
-    sendResponseTime: true,
-    logRequset: true,
-    requestTimeout: 10*1000, // request timeout 默认10s
-    requestTimeoutCallback: ()=>{}
-} 
  
-export default function(options) {
-    options = Object.assign({}, defaultOption, options);
-    
+module.exports = function (options) {
+    options = options || {};
     return async function meta(ctx, next) {
-        // logging TODO: nomi logger使用
-        if (options.logRequset) {
-            ctx.coreLogger.logger('[meta] request started, host: %s, user-agent: %s',
+        // logging
+        if (options.logging) {
+            console.log('[meta] request started, host: %s, user-agent: %s',
                 ctx.host, ctx.header['user-agent']);
         }
-
-        // set request timeout
-        ctx.response.setTimeout(options.requestTimeout, options.requestTimeoutCallback);
         
         let start =Date.now();
         await next();
         let end = Date.now();
-        // total response time header TODO: nomi中ctx.set()方法，此处是设置response headers
-        if (options.sendResponseTime) {
-            ctx.set('x-readtime', `${end - start}ms`);
+        // total response time header 
+        ctx.set('X-Response-Time', `${end - start}ms`);
+        if (options.logRequset) {
+            console.log(`${ctx.method} ${ctx.url} ${ctx.status} ${endTime - startTime}ms`);
         }
-
-        // try to support Keep-Alive Header TODO: server获取
+        
+        // try to support Keep-Alive Header   
         const server = ctx.app.server;
         if (server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
             const timeout = parseInt(server.keepAliveTimeout / 1000);
---
+++ test/meta.test.js
@@ -0,0 +1,99 @@
+/**!
+ * nomi-meta - test/middleware.test.js
+ */
+
+ "use strict";
+
+ /** 
+  * Module dependencies
+ */
+const assert = require('assert');
+const meta = require('../');
+const sinon = require('sinon');
+const Koa = require('koa');
+const request = require('supertest');
+
+let sandbox,log,app;
+describe('meta', ()=>{
+    describe('default config',async ()=> {
+        beforeEach(()=> {
+            app =App();
+        })
+        
+        it('should get X-Response-Time header', () => {
+            return request(app.listen())
+                .get('/')
+                .expect('X-Response-Time', /\d+/)
+                //.expect(200);
+        });
+    });
+
+    describe('meta.logging = true', ()=> {
+        app = App({logging: true});
+
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create()
+            log = sandbox.spy(console, 'log')
+        });
+
+        afterEach(function () {
+            sandbox.restore()
+        });
+
+        it('should log a request start', () => {
+            return request(app.listen())
+                .get('/')
+                .expect(404, ()=> {
+                    expect(log).to.have.been.calledWith('[meta] request started, host: %s, user-agent: %s',
+                        ctx.host,ctx.header['user-agent'])
+                });
+        });
+    });
+
+    describe('meta.logRequest = true', () => {
+        app = App({ logRequest: true });
+
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create()
+            log = sandbox.spy(console, 'log')
+        });
+
+        afterEach(function () {
+            sandbox.restore()
+        });
+
+        it('should log a request', () => {
+            return request(app.listen())
+                .get('/')
+                .expect(404, () => {
+                    expect(log).to.have.been.
+                        calledWith(`${ctx.method} ${ctx.url} ${ctx.status} ${endTime - startTime}ms`)
+                });
+        });
+    });
+
+    describe('cluster start', () => {
+        it('should ignore keep-alive header when request is not keep-alive', () => {
+            return request(app.listen())
+                .get('/')
+                .expect('X-Response-Time', /\d+/)
+                .expect(res => assert(!res.headers['keep-alive']))
+                //.expect(200);
+        });
+        it('should get keep-alive header when request is keep-alive', () => {
+            return request(app.listen())
+                .get('/')
+                .set('connection', 'keep-alive')
+                .expect('X-Response-Time', /\d+/)
+                .expect('keep-alive', 'timeout=5')
+            //.expect(200);
+        }); 
+    });
+
+});
+
+function App(options) {
+    const app = new Koa();
+    app.use(meta(options));
+    return app;
+}
